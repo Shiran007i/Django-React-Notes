3cffff9c85a3da5a41d3ab77547bb77b0a8da515
Shiran <athukorala007i@gamil.com>
2025-06-17 12:18:16 +0530

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,23 @@
-import axios from "axios";
-import { ACCESS_TOKEN } from "./constants";
+import axios from 'axios';
+import { ACCESS_TOKEN } from './constants';
+
+const apiUrl = '/choreo-apis/backend-454513563:8000';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(
+  (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
